823a79e3e199fbb56456d65d896bf9a050e99d41
Peyton Riley <psriley@ksu.edu>
2023-08-29 23:43:34 -0500

    Get primitive title screen setup

--- Thief-Repo-Man/Game1.cs
+++ Thief-Repo-Man/Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Diagnostics;
 
 namespace Thief_Repo_Man
 {
@@ -8,6 +9,17 @@ namespace Thief_Repo_Man
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+
+        private Texture2D playerTexture;
+        private Texture2D carTexture;
+        private Vector2 playerPosition;
+
+        private Vector2 car1Position;
+        private Vector2 car2Position;
+        private Vector2 car3Position;
+
+        private InputManager inputManager;
+        private SpriteFont inkFree;
 
         public Game1()
         {
@@ -19,7 +31,24 @@ namespace Thief_Repo_Man
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            playerPosition = new Vector2(
+                GraphicsDevice.Viewport.Width / 1.2f,
+                GraphicsDevice.Viewport.Height / 4
+            );
+            car1Position = new Vector2(
+                GraphicsDevice.Viewport.Width / 4,
+                GraphicsDevice.Viewport.Height / 3.75f
+            );
+            car2Position = new Vector2(
+                GraphicsDevice.Viewport.Width / 4,
+                GraphicsDevice.Viewport.Height / 1.5f
+            );
+            car3Position = new Vector2(
+                GraphicsDevice.Viewport.Width / 1.6f,
+                GraphicsDevice.Viewport.Height / 2
+            );
 
+            inputManager = new InputManager();
             base.Initialize();
         }
 
@@ -28,14 +57,19 @@ namespace Thief_Repo_Man
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            playerTexture = Content.Load<Texture2D>("player(drawn)");
+            carTexture = Content.Load<Texture2D>("car");
+
+            inkFree = Content.Load<SpriteFont>("inkFree");
         }
 
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
+            inputManager.Update(gameTime);
+            if (inputManager.Exit) Exit();
 
             // TODO: Add your update logic here
+            playerPosition += inputManager.Direction;
 
             base.Update(gameTime);
         }
@@ -45,6 +79,17 @@ namespace Thief_Repo_Man
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(playerTexture, playerPosition, Color.White);
+            _spriteBatch.Draw(carTexture, car1Position, Color.White);
+            _spriteBatch.Draw(carTexture, car2Position, Color.White);
+            _spriteBatch.Draw(carTexture, car3Position, Color.White);
+            _spriteBatch.DrawString(inkFree, $"Thief Repo Man", new Vector2(2, 2), Color.Gold, 0, Vector2.Zero, 4, SpriteEffects.None, 0);
+            _spriteBatch.DrawString(inkFree, $"Play", car1Position, Color.Gold, 0, Vector2.Zero, 2, SpriteEffects.None, 0);
+            _spriteBatch.DrawString(inkFree, $"Exit", car2Position, Color.Gold, 0, Vector2.Zero, 2, SpriteEffects.None, 0);
+            _spriteBatch.DrawString(inkFree, $"Options", car3Position, Color.Gold, 0, Vector2.Zero, 2, SpriteEffects.None, 0);
+            _spriteBatch.DrawString(inkFree, $"Use 'WASD' to move and 'E' to repo vehicles", new Vector2(GraphicsDevice.Viewport.Width / 7, GraphicsDevice.Viewport.Height / 1.1f), Color.Gold, 0, Vector2.Zero, 2, SpriteEffects.None, 0);
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
---
+++ Thief-Repo-Man/InputManager.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thief_Repo_Man
+{
+    public class InputManager
+    {
+        KeyboardState currentKeyboardState;
+        KeyboardState priorKeyboardState;
+        MouseState currentMouseState;
+        MouseState priorMouseState;
+        GamePadState currentGamePadState;
+        GamePadState priorGamePadState;
+
+        /// <summary>
+        /// The current direction
+        /// </summary>
+        public Vector2 Direction { get; private set; }
+
+        /// <summary>
+        /// If the user has request to end the game
+        /// </summary>
+        public bool Exit { get; private set; } = false;
+
+        /// <summary>
+        /// If the sprite needs to be flipped horizontally
+        /// </summary>
+        public bool hflipped = false;
+
+        /// <summary>
+        /// If the sprite needs to be flipped vertically
+        /// </summary>
+        public bool vflipped = false;
+
+        public void Update(GameTime gameTime)
+        {
+            #region Updating input state
+
+            priorKeyboardState = currentKeyboardState;
+            currentKeyboardState = Keyboard.GetState();
+
+            priorMouseState = currentMouseState;
+            currentMouseState = Mouse.GetState();
+
+            priorGamePadState = currentGamePadState;
+            currentGamePadState = GamePad.GetState(0);
+
+            #endregion
+
+            #region Direction input
+
+            // Get position from the GamePad
+            Direction = currentGamePadState.ThumbSticks.Right * 100 * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            // Get position from the Keyboard
+            if (currentKeyboardState.IsKeyDown(Keys.Left) ||
+                currentKeyboardState.IsKeyDown(Keys.A))
+            {
+                Direction += new Vector2(-100 * (float)gameTime.ElapsedGameTime.TotalSeconds, 0);
+            }
+            if (currentKeyboardState.IsKeyDown(Keys.Right) ||
+                currentKeyboardState.IsKeyDown(Keys.D))
+            {
+                Direction += new Vector2(100 * (float)gameTime.ElapsedGameTime.TotalSeconds, 0);
+            }
+            if (currentKeyboardState.IsKeyDown(Keys.Up) ||
+                currentKeyboardState.IsKeyDown(Keys.W))
+            {
+                Direction += new Vector2(0, -100 * (float)gameTime.ElapsedGameTime.TotalSeconds);
+            }
+            if (currentKeyboardState.IsKeyDown(Keys.Down) ||
+                currentKeyboardState.IsKeyDown(Keys.S))
+            {
+                Direction += new Vector2(0, 100 * (float)gameTime.ElapsedGameTime.TotalSeconds);
+            }
+
+            #endregion
+
+            #region Exit input
+
+            if (currentGamePadState.Buttons.Back == ButtonState.Pressed || currentKeyboardState.IsKeyDown(Keys.Escape))
+            {
+                Exit = true;
+            }
+
+            #endregion
+        }
+    }
+}
