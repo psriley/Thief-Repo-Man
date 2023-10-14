943183f7b67c34ecd13a826fdd2732cb4c76d24d
Peyton Riley <psriley@ksu.edu>
2023-10-13 19:21:29 -0500

    Simplify car controller for now

--- Thief-Repo-Man/CarController.cs
+++ Thief-Repo-Man/CarController.cs
@@ -16,6 +16,14 @@ namespace Thief_Repo_Man
 {
     public class CarController
     {
+        private float accelerationSpeed = 30.0f;
+        private float turnSpeed = 3.5f;
+
+        float acceleration = 0;
+        float steering = 0;
+
+        float rotationAngle = 0;
+
         private static Vector2 _direction;
         /// <summary>
         /// Direction that the player is facing.
@@ -26,11 +34,13 @@ namespace Thief_Repo_Man
         private float speed = 200f;
         private BoundingRectangle bounds;
         private float playerScale = 1.5f;
+        private float rotation = 0;
 
         public BoundingRectangle Bounds => bounds;
 
-        const float LINEAR_ACCELERATION = 10;
+        const float LINEAR_ACCELERATION = 100;
         const float ANGULAR_ACCELERATION = 2;
+        const float MAX_SPEED = 5;
 
         Game game;
         Texture2D texture;
@@ -46,6 +56,11 @@ namespace Thief_Repo_Man
             this.position = new Vector2(526, 356);
             this.bounds = new BoundingRectangle(playerPosition, (44 * playerScale), (22 * playerScale));
             _direction = -Vector2.UnitY;
+            angle = (float)Math.PI;
+            direction.X = (float)Math.Cos(angle);
+            direction.Y = (float)Math.Sin(angle);
+            Debug.WriteLine($"Initial angle: {angle}");
+            Debug.WriteLine($"Initial direction: {direction}");
         }
 
         public void HandleInput(GameTime gameTime, KeyboardState keyboardState)
@@ -56,37 +71,58 @@ namespace Thief_Repo_Man
             float angularAcceleration = 0;
             if (keyboardState.IsKeyDown(Keys.A))
             {
-                acceleration += direction * LINEAR_ACCELERATION;
-                angularAcceleration += ANGULAR_ACCELERATION;
+                angle -= 0.1f;
+                //acceleration += direction * LINEAR_ACCELERATION;
+                //angularAcceleration += ANGULAR_ACCELERATION;
             }
             if (keyboardState.IsKeyDown(Keys.D))
             {
-                acceleration += direction * LINEAR_ACCELERATION;
-                angularAcceleration -= ANGULAR_ACCELERATION;
+                angle += 0.1f;
+                //acceleration += direction * LINEAR_ACCELERATION;
+                //angularAcceleration -= ANGULAR_ACCELERATION;
             }
             if (keyboardState.IsKeyDown(Keys.W))
             {
-                acceleration += direction * LINEAR_ACCELERATION;
+                velocity -= direction * speed;
+                //acceleration += direction * LINEAR_ACCELERATION;
             }
             if (keyboardState.IsKeyDown(Keys.S))
             {
-                acceleration -= direction * LINEAR_ACCELERATION;
+                velocity += direction * speed;
+                //acceleration -= direction * (LINEAR_ACCELERATION * 2);
             }
+
+            //if (velocity.Length() > 0f)
+            //{
+            //    acceleration -= direction * (LINEAR_ACCELERATION / 2);
+            //}
             //if (acceleration.Length() > 0)
             //{
             //    acceleration -= direction * LINEAR_ACCELERATION;
             //    velocity -= acceleration * t;
             //}
 
-            angularVelocity += angularAcceleration * t;
-            angle += angularVelocity * t;
-            direction.X = (float)Math.Sin(angle - 90);
-            direction.Y = (float)-Math.Cos(angle - 90);
+            //angularVelocity += angularAcceleration * t;
+            //angle += angularVelocity * t;
+            direction.X = (float)Math.Cos(angle);
+            direction.Y = (float)Math.Sin(angle);
+            direction.Normalize();
 
-            velocity += acceleration * t;
+            //velocity += acceleration * t;
             position += velocity * t;
 
+            speed = MathHelper.Clamp(speed, 0.0f, MAX_SPEED);
+
+            ApplyEngineForce();
+            //ApplySteering();
+
             //Vector2.Clamp(acceleration, Vector2.Zero, Vector2.One);
+        }
+
+        void ApplyEngineForce()
+        {
+            // Create a force for the engine
+            Vector2 engineForceVector = new Vector2((float)Math.Sin(angle - 90), 0);
         }
 
         /// <summary>
